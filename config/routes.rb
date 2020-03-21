6d1095e3d112cb4176e2a1a3649c525b03a96cb8
REI OKADA <rei@REInoMacBook-Pro.local>
2020-03-21 13:46:30 +0900

    [Finish]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,9 @@ Rails.application.routes.draw do
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
   #更新の場合はpatch
+
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
+
+#HTTPメソッドのデフォルト値は、メソッドでそれぞれ決まっています。
+#link_toとredirect_toはGET、form_forはPOSTです。
